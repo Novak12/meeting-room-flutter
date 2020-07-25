b9ed3bced89702c6efc407af57897dd3c58d0c8f
yu.yi <novak12@163.com>
2020-07-26 00:02:54 +0800

    Update view to home page

--- meeting_room/lib/widgets/homepage.dart
+++ meeting_room/lib/widgets/homepage.dart
@@ -56,7 +56,91 @@ class _HomePageState extends State<HomePage> {
                 onTap: () => sortBy(sortBy: cases),
               ),
             ]),
-        body: HomePageListView());
+        body: new Scrollbar(
+            child: new ListView.separated(
+                itemBuilder: (context, item) {
+                  return _myListWidget(context, titleItems[item],
+                      iconItems[item], subTitleItems[item]);
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    new Divider(),
+                itemCount: iconItems.length)));
+  }
+
+  List<String> titleItems = <String>[
+    'Beijing',
+    'Munich',
+    'Shanghai',
+    'Tokyo',
+    'Chicago',
+    'Now York',
+    'Berlin',
+    'London',
+    'Paris',
+    'Wuhan',
+  ];
+
+  List<Icon> iconItems = <Icon>[
+    new Icon(Icons.keyboard),
+    new Icon(Icons.print),
+    new Icon(Icons.router),
+    new Icon(Icons.pages),
+    new Icon(Icons.zoom_out_map),
+    new Icon(Icons.zoom_out),
+    new Icon(Icons.youtube_searched_for),
+    new Icon(Icons.wifi_tethering),
+    new Icon(Icons.wifi_lock),
+    new Icon(Icons.widgets),
+    new Icon(Icons.weekend),
+    new Icon(Icons.web),
+    new Icon(Icons.accessible),
+    new Icon(Icons.ac_unit),
+  ];
+
+  List<String> subTitleItems = <String>[
+    'Changzhi 5F',
+    'Changzhi 5F',
+    'Changzhi 5F',
+    'Changzhi 5F',
+    'Changzhi 5F',
+    'Changzhi 6F',
+    'Changzhi 6F',
+    'Changzhi 6F',
+    'sChangzhi 6F',
+    'Changzhi 6F',
+    'Changzhi 6F',
+  ];
+
+  Widget _myListWidget(BuildContext context, String titleItem, Icon iconItem,
+      String subTitleItem) {
+    return new ListTile(
+      leading: iconItem,
+      title: new Text(
+        titleItem,
+        style: TextStyle(fontSize: 18),
+      ),
+      subtitle: new Text(
+        subTitleItem,
+      ),
+      trailing: new Icon(Icons.keyboard_arrow_right),
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return new AlertDialog(
+              title: new Text(
+                'ListViewAlert',
+                style: new TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              ),
+              content: new Text('您选择的item内容为:$titleItem'),
+            );
+          },
+        );
+      },
+    );
   }
 
   Future<void> sortBy({String sortBy}) async {
