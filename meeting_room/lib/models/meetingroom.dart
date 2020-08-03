1fff86cf347608b51c767c28f5c78da836f35e9d
yu.yi <novak12@163.com>
2020-08-03 22:09:19 +0800

    correct spell mistakes

--- meeting_room/lib/models/meetingroom.dart
+++ meeting_room/lib/models/meetingroom.dart
@@ -10,44 +10,44 @@ class MeetingRoom {
   MeetingRoom({this.name, this.image, this.status, this.location});
 
   static List<MeetingRoom> allRooms() {
-    var roomes = List<MeetingRoom>();
+    var rooms = List<MeetingRoom>();
 
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "Delhi",
       location: "Changzhi 5F",
       status: "今日可订",
       image: "assets/delhi.png",
     ));
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "London",
       location: "Changzhi 5F",
       status: "今日可订",
       image: "assets/london.png",
     ));
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "Shanghai",
       location: "Changzhi 6F",
       status: "今日已满",
       image: "assets/shanghai.png",
     ));
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "New York",
       location: "Changzhi 6F",
       status: "今日已满",
       image: "assets/nowyork.png",
     ));
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "Chicago",
       location: "Changzhi 6F",
       status: "今日已满",
       image: "assets/chicago.png",
     ));
-    roomes.add(MeetingRoom(
+    rooms.add(MeetingRoom(
       name: "Berlin",
       location: "Changzhi 6F",
       status: "今日已满",
       image: "assets/berlin.png",
     ));
-    return roomes;
+    return rooms;
   }
 }
