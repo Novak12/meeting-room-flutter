d1c4d178d1bf1a381742bf0ba7c9842d26be88e5
yi.yu <novak12@163.com>
2020-09-24 22:20:19 +0800

    add dart test demo

---
+++ test-demo/lib/test/test.dart
@@ -0,0 +1,14 @@
+void main() {
+  var str = 'hello dart';
+  var str1 = """hello
+  hihi 
+  world""";
+  print(str);
+  print(str1);
+  print("$str $str1");
+
+  //maps类型
+
+  var person = {"name": "Jim", "age": 21};
+  print(person["name"]);
+}
