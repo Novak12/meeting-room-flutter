7fd52af3aaaac2417951f168987912b7360aaca9
yi.yu <novak12@163.com>
2020-09-24 22:20:37 +0800

    Add http request demo

---
+++ meeting_room/lib/proxy/httpproxy.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+
+class HttpProxy {
+  void get() async {
+    var url = 'https://httpbin.org/ip';
+    var httpClient = new HttpClient();
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        var json = await response.transform(utf8.decoder).join();
+        var data = jsonDecode(json);
+        result = data['origin'];
+      } else {
+        result =
+            'Error getting IP address:\nHttp status ${response.statusCode}';
+      }
+    } catch (exception) {
+      result = 'Failed getting IP address';
+    }
+  }
+}
