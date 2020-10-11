4c5fbef07181243c9ce36ffaf07421f877cfd492
yi.yu <novak12@163.com>
2020-10-11 21:55:42 +0800

    add test demo

---
+++ meeting_room/test/materialwidgettest.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EvansYi"),
+          elevation: 6.0,
+        ),
+        body: Hello(),
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text('hello'),
+    );
+  }
+}
